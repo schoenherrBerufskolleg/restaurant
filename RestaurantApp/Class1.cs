5b4b2cd223366adc3a9f170f726ad284deef5b7c
Nova Mane <73277190+Nova03@users.noreply.github.com>
2023-10-17 11:18:23 +0200

    Add files via upload

---
+++ RestaurantApp/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace RestaurantApp
+{
+     class DatabaseManager
+    {
+        readonly string databasePath = "Data Source=C:\\Users\\MBösert\\Desktop\\projekt\\restaurant-main\\Sql\\Restaurant";
+        SQLiteConnection conn;
+
+        public void Connect()
+        {
+            conn = new SQLiteConnection(databasePath);
+            conn.Open();
+        }
+
+        public void Disconnect()
+        {
+            conn.Close();
+        }
+
+        public void ExecuteCommand(string command)
+        {
+            SQLiteCommand cmd = conn.CreateCommand();
+            cmd.CommandText = command;
+            Console.WriteLine("Amount of columns affected: " + cmd.ExecuteNonQuery());
+        }
+
+        public void ExecuteQuery(string query)
+        {
+            SQLiteCommand cmd = conn.CreateCommand();
+            cmd.CommandText = query;
+            SQLiteDataReader reader = cmd.ExecuteReader();   
+            while(reader.Read())
+            {
+                int id = reader.GetInt32(0);
+                string name = reader.GetString(1); 
+
+                Console.WriteLine($"ID: {id}, Name: {name}");
+            }
+        }
+    }
+}
--- RestaurantApp/Program.cs
+++ RestaurantApp/Program.cs
@@ -1,3 +1,5 @@
+using RestaurantApp;
+
 namespace WinFormsApp1
 {
     internal static class Program
@@ -8,10 +10,19 @@ namespace WinFormsApp1
         [STAThread]
         static void Main()
         {
+            DatabaseManager manager = new DatabaseManager();
+            manager.Connect();
+            string command = "INSERT INTO Employee (FirstName, LastName, Username, password, Role) VALUES(\'Maik\', \'Bösert\', \'Nova Mane\', \'test123\', \'Waiter\')";
+            string query = "SELECT * FROM Employee";
+            manager.ExecuteQuery(query);
+            //manager.ExecuteCommand(command);
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+            
         }
     }
 }
